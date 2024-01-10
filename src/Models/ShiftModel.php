7c9da3132de4db6d563a4f6e8208d984e8660a39
Tjalling Feenstra <tjallingdokkum@gmail.com>
2024-01-10 13:43:18 +0100

    update

--- src/Models/ShiftModel.php
+++ src/Models/ShiftModel.php
@@ -12,11 +12,16 @@
             'modified'         => ['modified.datetime', Model::TYPE_DATETIME],
             'department.id'    => 'department.department_id',
             'department.name'  => 'department.department_name',
-            'department.color' => 'department.color'
+            'department.color' => 'department.color',
+            'breaks'           => ['breaks', Model::CUSTOM]
         ];
 
-        public static function custom__department(int $id, array $data, array $input): array {
-            return [];
+        public static function custom__breaks(array $breaks, array $data, array $input): array {
+            return array_filter(array_map(function($break) {
+                list($hrs, $mins) = explode(":", $break['duration']);
+                $float_duration = (int)$hrs + ((int)$mins / 60);
+                return $float_duration;
+            }, $breaks));
         }
     }
 ?>
